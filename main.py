35d4c767ea8eb6244cf5a647a62aa0a09ca3fff6
AndersonBatalha <andersonpbatalha@gmail.com>
2019-04-16 11:42:49 -0300

    usando o pacote flask_sqlalchemy

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 from flask import Flask, render_template, request, session, url_for, redirect, flash
 from flask_bootstrap import Bootstrap
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__name__))
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'uma chave qualquer'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app=app, db=db)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' %(self.name)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' %(self.username)
 
 """
 Tipos de dados
