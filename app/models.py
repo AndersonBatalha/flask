625f8cfeeffccad5b96a9e61856e3543e28ad143
AndersonBatalha <andersonpbatalha@gmail.com>
2019-05-22 11:51:03 -0300

    Permissões de usuários

--- app/models.py
+++ app/models.py
@@ -3,14 +3,75 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    permission = db.Column(db.Integer)
     users = db.relationship('User', backref='role')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permission is None:
+            self.permission = 0
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+    def has_permission(self, perm):
+        return self.permission & perm == perm
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permission += perm
+
+    def remove_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permission -= perm
+
+    def reset_permission(self):
+        self.permission = 0
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            "User": [
+                Permission.FOLLOW,
+                Permission.COMMENT,
+                Permission.WRITE,
+            ],
+            "Moderator": [
+                Permission.FOLLOW,
+                Permission.COMMENT,
+                Permission.WRITE,
+                Permission.MODERATE,
+            ],
+            "Administrator": [
+                Permission.FOLLOW,
+                Permission.COMMENT,
+                Permission.WRITE,
+                Permission.MODERATE,
+                Permission.ADMIN,
+            ],
+        }
+        default_role = "User"
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permission()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
